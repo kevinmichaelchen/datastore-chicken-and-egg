e541957255ef9c0e6e8bd1356a369e58c5ebaf24
Kevin Chen <kevinmichaelchen@gmail.com>
2019-04-10 11:40:36 -0400

    Permissions example

--- main.go
+++ main.go
@@ -7,15 +7,16 @@ import (
 	"log"
 )
 
-const FolderEntity = "Folder"
+const (
+	PermissionsEntity = "Permissions"
+)
 
-type Folder struct {
-	ID       string
-	ParentID string
+type Permissions struct {
+	NodeID string
 }
 
-func (f Folder) String() string {
-	return fmt.Sprintf("Folder[ID=%s, ParentID=%s]", f.ID, f.ParentID)
+func (p Permissions) String() string {
+	return fmt.Sprintf("Permissions[NodeID=%s]", p.NodeID)
 }
 
 func main() {
@@ -30,40 +31,40 @@ func main() {
 		log.Fatalf("Error creating client. %s", err.Error())
 	}
 
-	// Let's make a root folder!!!
-	rootFolderID := "1"
-	rootFolderKey := datastore.NameKey(FolderEntity, rootFolderID, nil)
-	rootFolder := &Folder{
-		ID:       rootFolderID,
-		ParentID: "",
-	}
-	_, err = client.Put(ctx, rootFolderKey, rootFolder)
+
+	// Grant User 5 permissions on Node 1
+	n1 := "1"
+	k1 := datastore.NameKey(PermissionsEntity, n1, datastore.NameKey("User", "5", nil))
+	_, err = client.Put(
+		ctx,
+		k1,
+		&Permissions{NodeID: n1})
 	if err != nil {
-		log.Fatalf("Error putting root folder. %s", err.Error())
+		log.Fatalf("Error creating permissions: %s", err.Error())
 	}
 
-	// Let's make a subfolder under
-	// the root folder we just created!!!
-	subFolderID := "2"
-	subFolderKey := datastore.NameKey(FolderEntity, subFolderID, rootFolderKey)
-	subFolder := &Folder{
-		ID:       subFolderID,
-		ParentID: rootFolderID,
-	}
-	_, err = client.Put(ctx, subFolderKey, subFolder)
+	// Grant Team 6 permissions on Node 1
+	n2 := "45"
+	k2 := datastore.NameKey(PermissionsEntity, n2, datastore.NameKey("Team", "6", nil))
+	_, err = client.Put(
+		ctx,
+		k2,
+		&Permissions{NodeID: n2})
 	if err != nil {
-		log.Fatalf("Error putting subfolder. %s", err.Error())
+		log.Fatalf("Error creating permissions: %s", err.Error())
 	}
 
 	// OKAY!
 	// Let's do a DB lookup
 	//badKey := datastore.NameKey(FolderEntity, subFolderID, nil)
-	goodKey := datastore.NameKey(FolderEntity, subFolderID, rootFolderKey)
-	var f Folder
-	err = client.Get(ctx, goodKey, &f)
-	if err != nil {
+	var p1, p2 Permissions
+	if err := client.Get(ctx, k1, &p1); err != nil {
+		log.Fatalf("Error reading: %s", err.Error())
+	}
+	if err := client.Get(ctx, k2, &p2); err != nil {
 		log.Fatalf("Error reading: %s", err.Error())
 	}
 
-	log.Println("GET RESULT =", f.String())
+	log.Println("p1 =", p1.String())
+	log.Println("p2 =", p2.String())
 }
